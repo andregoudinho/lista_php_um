da5e2cda8b82b17bbc99069ba3c686eb50d747cb
andregoudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-07-07 11:47:39 -0300

    add - ex_01

---
+++ ex_01.php
@@ -0,0 +1,21 @@
+<?php
+
+function calcularFormula($x,$y){
+    if(($x + $y)==0){
+        return "Não é possível realizar a divisão";
+    }
+
+    $resultado = (pow($x,2) + pow($y,2)) / ($x + $y);
+    return $resultado;
+}
+$x=10;
+$y=5;
+
+echo "Valor de x: $x <br>";
+echo "Valor de y: $y <br>";
+echo "Resultado: ".calcularFormula($x,$y);
+
+
+
+
+?>
